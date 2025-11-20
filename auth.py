40b04acb1517cc148e84c5646120cbd26b90dfba
kaarthikswork <kaarthiksaicharan@gmail.com>
2025-11-20 21:51:28 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,37 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required, current_user
+from models import User
 
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.find_by_username(username)
+        if user and User(username, '').check_password(password):  # Dummy User for check
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+        flash('Invalid credentials')
+    return render_template('login.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.find_by_username(username):
+            flash('Username exists')
+        else:
+            user = User(username, password)
+            user.save()
+            login_user(user)
+            return redirect(url_for('main.dashboard'))
+    return render_template('register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
